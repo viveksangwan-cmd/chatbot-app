c5a56ab2ee25b80f1c8911cc0527771140472850
vivek <viveksangwan006@gmaiil.com>
2020-01-29 13:44:25 +0530

    day 2.2

--- testserver.py
+++ testserver.py
@@ -7,6 +7,9 @@ clients = {}
 grp_name_and_pass={}
 online_users=[]
 
+grp_name_and_users={}
+
+
 ss = socket.socket()
 port_number = 8000
 ss.bind(("", port_number))
@@ -16,9 +19,11 @@ print("Server is waiting for client.")
 def validate(group_name,group_password):
     if group_name in list(grp_name_and_pass.keys()):
         if grp_name_and_pass[group_name]==group_password:
-            print((" "*40)+str(group_name)+(" "*40))
             for i in online_users:
-                print(i,end="  ")
+                user=str(i)
+                conn.sendall(user.encode())
+            grp_name_and_users[group_name].append(usr_name)
+            print(grp_name_and_users,grp_name_and_users[group_name])
             return True
         else:
             wrong_password="Wrong Password"
@@ -29,32 +34,43 @@ def validate(group_name,group_password):
     else:
         grp_name_and_pass[group_name]=group_password
         print("Successful creation of group : " +str(group_name)+" : "+str(grp_name_and_pass[group_name] ))
+        grp_name_and_users[group_name]=[]
+        grp_name_and_users[group_name].append(usr_name)
+        print(grp_name_and_users,grp_name_and_users[group_name])
         return True
 
-def clients_task(client_name, conn, addr):
+def clients_task(usr_name, conn, addr):
     while True:
         data = conn.recv(1000)
-        message = client_name + ":" + data.decode()
-        for client in clients:
-            if client != client_name:
-                clients[client].sendall(message.encode())
-
+        message = usr_name + ":" + data.decode()
+        for users in grp_name_and_users[group_name]:
+            if users in online_users:
+                for client in clients:
+                    if client!=usr_name:
+                        print(users,usr_name)
+                        clients[users].sendall(message.encode())
+            else:
+                print("user is not online")
+                break
 
 while True:
     conn, addr = ss.accept()
     print("Server is connected to client at :", addr)
 
-    group_name=conn.recv(1000)
-    group_password=conn.recv(1000)
+    group_name=conn.recv(1000).decode()
+    group_password=conn.recv(1000).decode()
     print(group_name,group_password)
-    usr_name=conn.recv(1000)
+    usr_name=conn.recv(1000).decode()
     print(usr_name)
     if validate(group_name,group_password):
 
         online_users.append(usr_name)
+        print("online_users :",online_users)
+        clients[usr_name] = conn
 
-        clients[usr_name.decode()] = conn
-        t = Thread(target=clients_task, args=(usr_name.decode(), conn, addr))
+        print(clients)
+
+        t = Thread(target=clients_task, args=(usr_name, conn, addr))
         t.start()
     else:
-        print("Boobs")
+        print("--------------")
